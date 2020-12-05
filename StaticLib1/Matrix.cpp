a4513c97da98a3210c594a683f2930274eb8bd09
salah <salah@git.com>
2020-12-05 16:56:22 +0100

    deleteSomeCode

--- StaticLib1/Matrix.cpp
+++ StaticLib1/Matrix.cpp
@@ -43,8 +43,8 @@ void Matrix::remplir()
 	float number;
 	int i = 0;
 	
-	for (int i = 0; i < 3; i++) {
-		for (int j = 0; j < 3; j++) {
+	for (int i = 0; i < this->rows; i++) {
+		for (int j = 0; j < this->cols; j++) {
 			file >> number;
 			this->matrix[i][j] = number;
 		}
